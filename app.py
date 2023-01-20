57916b80abc44075a5894ae3e0117438a5306886
darkaoui hatim <timo32145@gmail.com>
2023-01-21 00:47:26 +0100

    last update on login

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import json
 import cx_Oracle
-from flask import Flask,request,app,jsonify,url_for,render_template,redirect
+from flask import Flask,request,app,jsonify,url_for,render_template,redirect,g
 from flask_restful import Api,Resource,reqparse
 from flask_cors import CORS,cross_origin
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
-
+import pickle
 
 app=Flask(__name__)
 
@@ -16,12 +16,12 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-session=None
-pool=None
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.get(user_id)
+
+
 
 class User(UserMixin):
     def __init__(self, id, username, password):
@@ -54,7 +54,21 @@ class LoginForm(FlaskForm):
 
     submit = SubmitField('Login')
 
+class sessionPool():
+      def __init__(self, username, password, inf):
+        self.conexion = cx_Oracle.connect(
+                  username,
+                password,
+                inf,
+                encoding = 'UTF-8' ) 
 
+      def getsession(self):
+        return self.conexion
+      def destroysesion(self):
+        cursor = self.conexion.cursor()
+        cursor.close()
+              
+'''
 def getsession(user,psw,inf):
       l=[]
       pool = cx_Oracle.SessionPool(
@@ -66,8 +80,11 @@ def getsession(user,psw,inf):
       l.append(pool)
       l.append(conexion)
       return l
+
 def endsession(conexion,pool):
     pool.release(conexion)
+'''
+
 
 def init():
      username = 'sys'
@@ -93,8 +110,6 @@ def getuser(user_name):
     return l
 
 class course():
-    def __init__(self,connexion):
-        self.__connexion=connexion
 
     def getconnection(self,score,score1):
         self.__connexion = cx_Oracle.connect(score,score1,'localhost:1521/orcl.mshome.net')
@@ -105,9 +120,7 @@ class course():
         p1='select * from cheffiliere.course'
         cursor.execute(p1)
         d={'course':list(json.dumps(x) for x in cursor)}
-        connexion.commit()
-        connexion.close()
-        connexion.close()  
+        connexion.commit() 
         return d  
 
     def insertdata(self,connexion):
@@ -119,32 +132,45 @@ class course():
         cursor.close()
         connexion.close()
         return "Data inserted successfully"   
-
+session_pool = None        
 @app.route('/login',methods=['POST','GET'])     
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        username=form.username.data
-        password= form.password.data
+    if request.method=='POST':
+        username=request.form["username"]
+        password= request.form["password"]
+        print(username,password)
         l=getuser(username)
         userid=l[1]
         if username==l[0]:
             if password=='123':  
+                p=sessionPool(username, password, 'localhost:/orcl.mshome.net')
+                global session_pool
+                session_pool=p.getsession()
+                print(session_pool)
                 new_user = User(l[1],username=form.username.data, password=form.password.data)
                 login_user(new_user)
                 print("hiiiiiiiiii",l[1])
                 return redirect(url_for('table'))
-    return render_template('test.html', form=form)
+    return render_template('login.html')
 
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
+    global session_pool
+    cursor = session_pool.cursor()
+    cursor.close()
     logout_user()
     return redirect(url_for('login'))
-@app.route('/table',methods=['POST','GET'])    
+@app.route('/table/',methods=['POST','GET'])    
 @login_required
 def table():
-    return render_template('table.html')
+    global session_pool
+    print(session_pool)
+    c=course()
+    d=c.selectdata(session_pool)
+    #print(c.selectdata(c))
+    return render_template('table.html',data=d)
     '''
         userinfo=[]
     if request.method=='POST':
@@ -165,7 +191,6 @@ def connexion(score,score1):
 @app.route('/')
 def home():
     return render_template('home.html')
-
 
 
 if __name__=="__main__":
